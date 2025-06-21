74cb23dddc4be090cedbf7600efa962c3585a08b
Tejas18 <tejas18.dev@gmail.com>
2025-06-21 23:42:10 +0530

    sentence grammar fixed

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -10,8 +10,8 @@ export default function Contact() {
                 </Sparkles>
             </div>
             <p className='contact-text'>
-                Have questions? Ping me with a <a href="https://www.linkedin.com/in/tejaschodankar18/" target='_blank'>linkedin. </a> 
-                DM or email at <a href='mailto:tejas.chodankar6@gmail.com' target='_blank'>tejas.chodankar6@gmail.com</a>.
+                Have questions? Ping me with a <a href="https://www.linkedin.com/in/tejaschodankar18/" target='_blank'>linkedin</a> 
+                . DM or email at <a href='mailto:tejas.chodankar6@gmail.com' target='_blank'>tejas.chodankar6@gmail.com</a>.
             </p>
         </div >
     )
